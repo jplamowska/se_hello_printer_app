d21ee6236edd9c17670b64ba45cc099f70ad0836
jplamowska <jplamowska@users.noreply.github.com>
2018-05-06 14:49:35 +0200

    Przykład z imionami

--- test-ui/test_ui.py
+++ test-ui/test_ui.py
@@ -9,11 +9,16 @@ class TestFormater(unittest.TestCase):
     def test_plain_lowercase(self):
         driver = webdriver.Chrome()
         driver.get("http://127.0.0.1:5000/ui")
-        self.do_valid_hello(driver)
+        self.do_valid_imie(driver)
         time.sleep(10)
         driver.quit()
 
-    def do_valid_hello(self, driver):
-        welcome = driver.find_element_by_id('Welcome')
-        print(welcome.text)
-        self.assertEqual(welcome.text, "Welcome")
+    def do_valid_imie(self, driver):
+        verify = driver.find_element_by_id('imie')
+        print(verify.text)
+        self.assertEqual(verify.text, "Justyna")
+
+    def go_google(self,driver):
+        mark = driver.find_element_by_partial_link_text('imiona')
+        print(mark)
+        mark.click()
